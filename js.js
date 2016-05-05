66fe100c5cf6237dced34d2a330aef02063d1caa
HoangHai1095 <thhai1312172@gmail.com>
2016-05-05 17:07:52 +0700

    Update js.js

--- js.js
+++ js.js
@@ -1,88 +1,43 @@
 
-angular.module('MyProfile', [])
-        .controller('ExampleController', ['$scope', function($scope)
+angular.module('MyProfile', ['ngSanitize'])
+        .controller('ExampleController', ['$scope','$http', function($scope, $http)
         {
-            // data
-            $scope.profile = {
-                src:'https://scontent-hkg3-1.xx.fbcdn.net/hphotos-xtp1/v/t1.0-9/12963518_1242886602407942_3988529011869897264_n.jpg?oh=895107f14950bb8e4006246a48de4f19&oe=57BEBBF9',
-                href:'https://www.facebook.com/be.bu.98'
-            };
-            $scope.profile_overview = {
-                name:'Hải Tô',
-                fullname:'Tô Hoàng Hải',
-                job:"university of science"
-            };
-            $scope.extra_info=[
-                {
-                    name:"current",
-                    content:"Student of University of Science"
-                },
-                {
-                   name:"previous",
-                   content:'Student of Phú Nhuận High School'
-                },
-                {
-                    name:"Education",
-                    content: "Student of university of science"
-                }
+            $http.get('json.json').success(function(data) {
+              $scope.data = data;
 
-            ];
-            $scope.aboutme = [
-                {
-                    name: "Email",
-                    content:"thhai1312172@gmail.com"
-                },
-                {
-                    name: "Phone",
-                    content:"01219960092"
-                },
-                {
-                    name: "Address",
-                    content:"277/23 Trường Chinh F.14 Q.Tân Bình Tp. Hồ Chí Minh"
-                }
-            ];
-            $scope.description={
-                content1:"Internet executive with over 20 years of experience, including general management of mid to large size organizations, corporate development, product development, business operations, and strategy.",
-                 content2:"Currently CEO at LinkedIn, the web's largest and most powerful network of professionals.",
-                content3:"Prior to LinkedIn, was an Executive in Residence at Accel Partners and Greylock Partners. Primarily focused on advising the leadership teams of the firm's existing consumer technology portfolio companies while also working closely with the firm’s partners to evaluate new investment opportunities.",
-                content4:"Previously served in key leadership roles at Yahoo! for over seven years, most recently as the Executive Vice President of Yahoo!'s Network Division managing Yahoo's consumer web product portfolio, including Yahoo's Front Page, Mail, Search, and Media products.",
-                content5:"Specialties: general management, corporate development, product development, business operations, strategy, product marketing, non-profit governance"
-                 };
-            $scope.position = [
-            {
-                href:"https://www.linkedin.com/company/linkedin?trk=ppro_cprof",
-                logo_img:"https://media.licdn.com/media/AAEAAQAAAAAAAAL0AAAAJGMwYWZhNTYxLWJkMTktNDAzMi05NzEzLTlhNzUxMGU0NDg0Mw.png",
-                title_href:"https://www.linkedin.com/title/ceo?trk=pprofile_title",
-                title_name:"CEO",
-                subtitle_name:"LinkedIn",
-                date_range:"December 2008 – Present (7 years 5 months)"
-            },
-             {
-                href:"https://www.linkedin.com/company/linkedin?trk=ppro_cprof",
-                logo_img:"https://media.licdn.com/media/p/2/000/205/177/3cfd5b9.png",
-                title_href:"https://www.linkedin.com/title/ceo?trk=pprofile_title",
-                title_name:"Member, Board of Directors",
-                subtitle_name:"Intuit",
-                date_range:"April 2012 – Present (4 years 1 month)"
-            },
-            {
-                href:"https://www.linkedin.com/company/linkedin?trk=ppro_cprof",
-                logo_img:"https://media.licdn.com/media/AAEAAQAAAAAAAAIEAAAAJGFhNGYyNjBjLWUxOWItNDRlYi05MTY0LTE5ZDA3YThhMTRkYg.png",
-                title_href:"https://www.linkedin.com/title/ceo?trk=pprofile_title",
-                title_name:"Member, Board of Directors",
-                subtitle_name:"DonorsChoose",
-                date_range:"2007 – Present (9 years)"
-            },
-             {
-                href:"https://www.linkedin.com/company/linkedin?trk=ppro_cprof",
-                logo_img:" https://media.licdn.com/media/p/5/005/00c/1bf/134d5ca.png",
-                title_href:"https://www.linkedin.com/title/ceo?trk=pprofile_title",
-                title_name:"Member, Board of Directors",
-                subtitle_name:"Boys & Girls Clubs of the Peninsula",
-                date_range:"2011 – Present (5 years)San Francisco Bay Area"
-            }
-            ];
+              function res(data){
+                 var s="";
+                 console.log(data);
+                  data.forEach(function(entry){s+=entry.content+"\n";});
+                 console.log(s);
+                  return s;
+              };
+
+              $scope.summarycontent=res($scope.data.summary.contents);
+              $scope.educationcontent=res($scope.data.education.contents);
+              $scope.educationcontent=res($scope.data.education.contents);
+              $scope.projectcontent=res($scope.data.project.contents);
+
+            }); 
+
+          
+
+
+           $scope.edit = {statusfullname:{name:"hello", status:false},
+                    statusjob:{name:"hello",  status:false},
+                    statushistory:{name:"hello",  status:false},
+                    statusinformationme:{name:"hello",  status:false},
+                    statesummary:{name:"hello",  status:false},
+                    stateeducation:{name:"hello",  status:false},
+                    stateproject:{name:"hello",  status:false},
+                    stateavatar:{name:"hello",  status:false},
+                    stateskill:{name:"hello",  status:false}
+                  };
+
+            $scope.hidetextbox = function(nameshow){
+              nameshow.status = !nameshow.status;
+              console.log(nameshow);   
+            };
+
+         
         }]);
-                        
- 
-                   
